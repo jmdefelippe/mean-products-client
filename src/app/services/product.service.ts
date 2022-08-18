b08cc13a9699e167fbc3d58ee3b04788c26d8b7e
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-08-18 13:52:14 -0300

    Added: Options Headers to Products requests

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,59 +6,38 @@ import { Product } from '../models/product';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
-  // ver variables de entorno: API = https://mean-products-server.herokuapp.com
-  // url = 'https://mean-products-server.herokuapp.com/api/products/';
-  url = 'http://localhost:4000/api/products/';
+  url = 'http://localhost:4000/api/products';
 
   constructor(private http: HttpClient) { }
 
+  private getOptions(): any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization:  `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`
+      })
+    };
+  }
+
   getProducts(): Observable<any> {
-    // console.log(localStorage.getItem("ACCESS_TOKEN"));
-
-    // this.http.get(this.url,{
-    //   headers: {'Authorization':`Bearer ${localStorage.getItem("ACCESS_TOKEN")}`}
-    // });
-    
-    let headers = new HttpHeaders();
-    headers = headers.set('Authorization',  `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`);
-
-    console.log(headers);
-
-
-    // const options = new RequestOptions({
-    //   headers: header,
-    // });
-    // return this._http.get(this.ApiURL + "api/Subscriptions/IsClientCreditCardExits/" + companyId + "/", options);    
-
-
-    
-    // this.http.get(this.url, { headers });
-    
-    // return fetch(this.url, {
-    //   method: 'GET',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Authorization': `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`
-    //   }
-    // });
-
-    // return this.http.get(this.url);
+    return this.http.get(this.url, this.getOptions());
   }
 
   deleteProduct(id: string): Observable<any> {
-    return this.http.delete(this.url + id);
+    return this.http.delete(this.url + id, this.getOptions());
   }
 
   createProduct(product: Product): Observable<any> {
-    return this.http.post(this.url, product);
+    return this.http.post(this.url, product, this.getOptions());
   }
 
   getProduct(id: string): Observable<any> {
-    return this.http.get(this.url + id);
+    return this.http.get(this.url + id, this.getOptions());
   }
 
   updateProduct(id: string, product: Product): Observable<any> {
-    return this.http.put(this.url + id, product);
+    return this.http.put(this.url + id, product, this.getOptions());
   }
 }
